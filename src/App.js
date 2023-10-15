ec5e751570035adc4ba7dbe049e1b18e568979ee
Anshu Kumar Bishwas <123227226+Anshu331@users.noreply.github.com>
2023-10-16 00:06:08 +0530

    A web application built with React for performing basic text operations. Features: Uppercase Conversion: Converts text to uppercase. Lowercase Conversion: Changes text to lowercase. Copy Text: Duplicates the text to the clipboard. Clear Text: Clears the text input. User-friendly interface with input fields and buttons for each operation. Real-time updates and interactivity using React's component-based architecture.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
+
+
 import './App.css';
 
+import Navbar from './components/Navbar';
+//import About from './components/About';
+import Textform from './components/Textform';
+import { useState } from 'react';
+import Alert from './components/Alert';
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [mode, setMode] = useState('light'); //wheather dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+   const showAlert = (message,type)=>{
+    setAlert({
+    msg: message,
+    type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },3000);
+  }
+
+const toggleMode = ()=>{
+  if(mode === 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor = '#042743';
+    showAlert("Dark mode has been enabled", "success");
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+    showAlert("light mode has been enabled", "success");
+  }
+}
+return (
+<>
+
+<Navbar title="Textutils" mode = {mode} toggleMode={toggleMode} />
+<Alert alert={alert}/>
+<div className= "container my-3">
+
+
+{/* <About/> */}
+<Textform showAlert={showAlert} heading = "Enter the text to analyze" />
+</div>
+
+</>
+        
   );
 }
-
+ 
 export default App;
